58b3c605c7cab4c4e17b85f59bc70e2f1c69ff5e
Simone Gentili <sensorario@gmail.com>
2017-05-03 19:36:24 +0200

    fix php 5.3 syntax

--- src/Sensorario/Container/Register.php
+++ src/Sensorario/Container/Register.php
@@ -7,7 +7,7 @@ use Sensorario\Container\Resolver\ResolverInterface;
 
 class Register
 {
-    private $instances = [];
+    private $instances = array();
 
     public function has(Service $service)
     {
